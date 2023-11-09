72f2859a8438f8d8c95457290983dfa9f5a97eaf
Ejiscoexpress <ejiscoexpress@gmail.com>
2023-11-09 16:41:27 +0100

    Bio

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import Gallery from "./Gallery.js";
-import Profile from "./Profile.js";
+export default function Bio () {
+    return (
+   <div>
+    <div className='intro'>
+        <h1>Welcome To EjicsTech Website</h1>
+        <p className='Summary'>
+            You can order for our services here.
+            <br /><br />
+            <b>And<i>Visit our Site to </i></b>Enjoy our Technological Services !
 
-export default function App () {
-    return(
-        
-        
-        <div>
-            <Profile />
-            <Gallery />
-        </div>
-        
+        </p>
+
+    </div>
+   </div>
     );
 }
