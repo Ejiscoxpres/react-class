086b84351c0197894e5112a0b88d172009ce67c1
Ejiscoexpress <ejiscoexpress@gmail.com>
2023-11-09 12:07:01 +0100

    import gallery

--- src/App.js
+++ src/App.js
@@ -1,31 +1,7 @@
-  function Profile() {
+import Gallery from "./Gallery.js";
+
+export default function App (){
     return(
-        <div>
-    <img
-     src="https://i.imgur.com/MK3eW3As.jpg"
-     alt="Susan Johnson" 
-    />
-   <img 
-     src="https://i.imgur.com/lICfvbD.jpg"
-     alt="Lekam Lekam" 
-   />
-
-    </div>
-    
-    );
-
-}
-
-export default function Gallery() {
-    return (
-        <section>
-            <h1>Great Secientist</h1>
-            <Profile />
-            <Profile />
-            <Profile />
-            <Profile />
-            <Profile />
-            
-        </section>
+        <Gallery />
     );
 }
---
+++ src/Gallery.js
@@ -0,0 +1,31 @@
+function Profile() {
+    return(
+        <div>
+    <img
+     src="https://i.imgur.com/MK3eW3As.jpg"
+     alt="Susan Johnson" 
+    />
+   <img 
+     src="https://i.imgur.com/lICfvbD.jpg"
+     alt="Lekam Lekam" 
+   />
+
+    </div>
+    
+    );
+
+}
+
+export default function Gallery() {
+    return (
+        <section>
+            <h1>Great Secientist</h1>
+            <Profile />
+            <Profile />
+            <Profile />
+            <Profile />
+            <Profile />
+            
+        </section>
+    );
+}
