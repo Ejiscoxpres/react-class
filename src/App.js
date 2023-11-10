9cb96a3d6ac517e9bda112490b14b944782d77d0
Ejiscoexpress <ejiscoexpress@gmail.com>
2023-11-10 16:16:32 +0100

    avatar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-export default function Avail () {
-    const avail='https://i.imgur.com/7vQD0fPs.jpg'
+export default function Avatar () {
+    const avatar='https://i.imgur.com/7vQD0fPs.jpg'
     const description='Hassan Emile'
     return(
         <img 
-        className='avail'
-        src= {avail}
+        className='avatar'
+        src= {avatar}
         alt={description}
         />
     );
