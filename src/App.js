7adf1d398f31cd4b381ee43bf80e0b78d04a0e82
Ejiscoexpress <ejiscoexpress@gmail.com>
2023-11-10 15:52:27 +0100

    className

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ export default function Fubara () {
     return(
         <img 
         className='fubara'
-        src='https://i. imgur.com/7vD0fPs.jpg'
+        src='https://i.imgur.com/7vQD0fPs.jpg'
         alt='Hassan Emile'
         />
     )
