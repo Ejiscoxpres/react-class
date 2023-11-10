fcd027b13050e0200faa7e4b0e17e2431aad9247
Ejiscoexpress <ejiscoexpress@gmail.com>
2023-11-10 16:54:26 +0100

    exprt default function

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
-const date= new Date();
+const theday= new Date();
 function formatDate(date) {
     return new Intl.DateTimeFormat(
         'en-Us',
         {weekday: 'long'}
     ).format(date);
+
+}
+export default function Daily(){
+    return(
+        <h1>Daily Activities For {formatDate(theday)}</h1>
+    )
 }
